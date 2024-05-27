408ab0c326519ba6dee65e74d8598e7a0519a572
Vadim Roschupkin <vroshupkin1@gmail.com>
2024-05-27 06:02:14 +0300

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,18 @@
 
-import { FC, useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.sass'
-import { ThemeCard } from './components/ThemeCard'
+import { ThemeCards } from './components/ThemeCards'
+import { ThemeSelector } from './components/ThemeSelector'
 
-
-type TApi = {
-  id: number,
-  name: string,
-  image: string,
-  bgColor: string,
-  tags: string[]
-}
-
-const api = async () => 
+function App() 
 {
-  const res = await fetch('https://logiclike.com/docs/courses.json')
-
-  return await res.json() as TApi[];
-}
-
-
-
-
-type PropsThemeRow = {children: string, selectValue: string, onClick: (val: string) => void}
-export const ThemeRow: FC<PropsThemeRow> = ({children, selectValue, onClick}) => 
-{
-  
-  return(
-     <div onClick={_ => onClick(children)}
-      className={'theme-row ' + (selectValue === children ? 'theme-row-select' : 'theme-row-non_select')}>
-        <div>
-          <span>{children}</span>
-        </div>
-      </div>
-  )
-}
-
-type PropsThemeSelector = {
-  onChange?: (v: string) => void  
-}
-export const ThemeSelector: FC<PropsThemeSelector> = () => {
-
-  const [selectVal, setSelectVal] = useState('Все темы')
-  
-  const imgUrl =
-   'https://s3.logiclike.com/s3/logic-training/side-courses/files/e6Prc6SlewvdmDhdvQgZiZkddqdLAhr1UPfVm366.png'
-  return (
-    <>
-    
-      <div className='theme-selector-container'>
-        {[
-          'Все темы', 'Логика и мышление', 'Загадки', 'Головоломки', 'Путешествия'
-        ].map(str => 
-          <ThemeRow onClick={setSelectVal} selectValue={selectVal}>{str}</ThemeRow>)
-        }
-      </div>
-
-      <ThemeCard 
-        imgUrl={imgUrl}
-        name='Угадай предмет'
-        bgColor='#977BEC'
-        />
-         
-
-    </>
-  )
-}
-
-
-function App() {
-
-
-  const [val, setVal] = useState<TApi[]>([])
-
-  useEffect(() => 
-  {
-    console.log(val);
-    api().then(setVal);
-  }, [])
-
+  const [filterVal, setFilterVal] = useState('');
 
   return (
     <>
       <div className='app-container'>
-        <ThemeSelector/>
+        <ThemeSelector onChange={setFilterVal}/>
+        <ThemeCards filterVal={filterVal}/>
       </div>
     </>
   )
--- src/components/ThemeCard.tsx
+++ src/components/ThemeCard.tsx
@@ -1,40 +1,26 @@
 import { FC } from 'react'
 import './ThemeCard.sass'
 
-
-
-// TODO Сделать shadow-course
 type PropsThemeCard = {
     imgUrl: string,
     name: string,
     bgColor: string
 }
+
 export const ThemeCard: FC<PropsThemeCard> = ({imgUrl, name, bgColor}) => {
-
-    const img_container = document.querySelector('.theme-card-img-container') as HTMLDivElement
-    if(img_container)
-    {
-        img_container.style.backgroundColor = bgColor
-    }
-
-    
   return (
     <div className='theme-card'>
-
-        <div className='theme-card-img-container'>
+        <div className='theme-card-img-container' style={{backgroundColor: bgColor}}>
             <div>
                 <img src={imgUrl} alt="" />
-            </div>
-            
+            </div>            
         </div>
 
         <div className='theme-card-name'>
             <div>
                 <span>{name}</span>
-            </div>
-            
-        </div>
-    
+            </div>            
+        </div> 
     </div>
   )
 }
